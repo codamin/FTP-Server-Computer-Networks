f6ad4246708650242a624baf83e7076c3f8012de
Amin Asadi <aminasadi329@gmail.com>
2020-04-05 21:13:24 +0430

    clean client code

--- client/client.py
+++ client/client.py
@@ -4,13 +4,10 @@ import json
 import time
 import sys
 import os
+from termcolor import colored
 
 sys.path.append(os.path.abspath('../'))
 from defs import *
-
-user_name = 'hoenza'
-password = '8585'
-RECV_SIZE = 1024
 
 def read_configs():
     configs = open(CONFIG_PATH).read()
@@ -18,32 +15,25 @@ def read_configs():
     return configs_dict
     
 
-class Client:
-    def __init__(self):
-        self.user_name = user_name
-        self.password = password
-        self.is_signed_in = False
-        
+class Client:        
     def connectToServer(self):
         self.command_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.command_sock.connect((HOST_IP, commandChannelPort))
 
-    def log_in(self):
-        if self.is_signed_in:
-            print("already singed in")
-            return
-        self.command_sock.sendall('USER {}'.format(self.user_name).encode('utf-8'))
-        res = self.command_sock.recv(RECV_SIZE).decode('utf-8').split(' ')
-        print(res)
-        if res[0] == NAME_OKAY_CODE:
-            self.command_sock.sendall('PASS {}'.format(self.password).encode('utf-8'))
-            res = self.command_sock.recv(RECV_SIZE).decode('utf-8').split(' ')
-            print(res)
-            if res[0] == LOG_IN_OKAY_CODE:
-                print('logged in successfully')
+    def send(self, msg):
+        self.command_sock.sendall(msg.encode('utf-8'))
 
+    def recv(self):
+        print(colored(self.command_sock.recv(RECV_SIZE).decode('utf-8'), 'red'))
 
+    def User(self, user_name):
+        self.send('USER {}'.format(user_name))
+        self.recv()
 
+    def Pass(self, password):
+        self.send('PASS {}'.format(password))
+        self.recv()
+        
 if __name__ == "__main__":
     # READ CONFIGS
     configs = read_configs()
@@ -51,10 +41,12 @@ if __name__ == "__main__":
 
     #CREATE CLIENT
     client = Client()
-
     client.connectToServer()
-    client.log_in()
-    #COMMANDS
-    # while(True):
-        # command = input('ENTER COMMAND: ')
 
+    #COMMANDS
+    while(True):
+        command = input('ENTER COMMAND: ').split(' ')
+        if command[0].lower() == 'user':
+            client.User(command[1])
+        if command[0].lower() == 'pass':
+            client.Pass(command[1])
